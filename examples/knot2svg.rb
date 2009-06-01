7f35b8a1e1a0897ddc39aa4a134018f4f298053d
Jamis Buck <jamis@37signals.com>
2009-06-01 09:25:16 -0600

    add -g option to knot2svg.rb to toggle display of the graph

--- examples/knot2svg.rb
+++ examples/knot2svg.rb
@@ -1,6 +1,7 @@
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), "..", "lib"))
 require 'celtic_knot'
 
+show_graph = ARGV.include?("-g")
 knot_file = ARGV.first or abort "please specify the knot file to read"
 
 graph = File.open(knot_file) { |file| CelticKnot::Graph.parse(file) }
@@ -14,8 +15,7 @@ File.open(svg_file, "w") do |svg|
   svg.puts '<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">'
   svg.puts '<svg width="5cm" height="5cm" version="1.1" xmlns="http://www.w3.org/2000/svg">'
 
-  # uncomment this line to include the original graph in the output
-  # svg.puts graph.to_svg(:color => "#fcc")
+  svg.puts graph.to_svg(:color => "#fcc") if show_graph
 
   svg.puts knot.to_svg(:width => 5, :fill => "#77f", :stroke => "#007")
   svg.puts '</svg>'
